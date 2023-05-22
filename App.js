a408ae138589bbc800ea8c1a03109c6376dd15a6
Chandra\nPrakash <chandraprakash47637@gmail.com>
2023-05-23 00:50:14 +0530

    ejs Set uped

--- App.js
+++ App.js
@@ -3,8 +3,18 @@
 
 const express = require('express');
 const app = express();
-
+const path = require('path');
 const port = 80;
+const ejsMate = require('ejs-mate');
+
+
+
+app.set('view engine', 'ejs');
+app.engine('ejs', ejsMate);
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true }))
 
 app.listen(port, () => {
     console.log('http://127.0.0.1:80/campground')
