a0574db14ae6b9130c088f6fc59516a98347ecf8
Chandra\nPrakash <chandraprakash47637@gmail.com>
2023-05-23 00:56:30 +0530

    mongoose setupped and method override liberary is added

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ const app = express();
 const path = require('path');
 const port = 80;
 const ejsMate = require('ejs-mate');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 
 
@@ -14,7 +16,27 @@ app.engine('ejs', ejsMate);
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+
+
+
+
+const Db_Url='mongodb://127.0.0.1:27017/Unusable2Usable';
+mongoose.connect(Db_Url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // strictQuery:true
+    // useCreateIndex:true
+    // useFindAndModify:true
+});
+const db = mongoose.Connection;
+mongoose.connection.on('error', console.error.bind(console, 'connection error'));
+mongoose.connection.once("open", () => {
+    console.log('Database Connected');
+});
+
 
 app.listen(port, () => {
     console.log('http://127.0.0.1:80/campground')
