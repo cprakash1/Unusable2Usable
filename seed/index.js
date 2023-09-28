637aef9efb618731b66afd93dbdea03ad0494f4a
Chandra\nPrakash <chandraprakash47637@gmail.com>
2023-09-28 21:07:06 +0530

    added search feature

--- seed/index.js
+++ seed/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Campground = require("../model/campground");
+const Campground = require("../model/items.js");
 const { descriptors, places } = require("./seedHelpers");
 const arra = require("./cities");
 const User = require("../model/user");
@@ -9,7 +9,7 @@ const geocoder = mbxgeocoding({
     "pk.eyJ1IjoiY3ByYWthc2gxIiwiYSI6ImNsZzZpNXBpMjBkZzkzaHFyMm83OGQyN3YifQ.5BnzbS1hsEGKg95hwpbQ7Q",
 });
 
-mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp", {
+mongoose.connect("mongodb://127.0.0.1:27017/Unusable2Usable", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -24,7 +24,7 @@ mongoose.connection.once("open", () => {
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 const createCampground = async () => {
   await Campground.deleteMany({});
-  const user = await User.findById("641ae7968c1f30acde15d15c");
+  const user = await User.findById("648b18c8e8f5105593c4c563");
   console.log(user);
   for (let i = 0; i < 10; i++) {
     const camp = new Campground({
@@ -32,12 +32,6 @@ const createCampground = async () => {
       location: `${arra[Math.floor(Math.random() * 1000)].city} ${
         arra[Math.floor(Math.random() * 1000)].state
       }`,
-      image: [{
-        url:"https://source.unsplash.com/collection/483251",
-        filename:"Empty"
-    }, {
-        url:"https://source.unsplash.com/collection/483251",
-        filename:"Empty"}],
       description:
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam, modi natus. Nobis eum, provident illo quaerat deleniti culpa consequuntur dolor ratione consectetur ea quis. Velit corrupti esse sit atque ratione?Consectetur neque recusandae, praesentium distinctio sed placeat odio et repudiandae vitae repellat sint officia magni at, a fuga ex, est quas! Saepe, quos nemo. Amet autem saepe animi quibusdam nostrum.Dolor voluptatem ipsam molestias quia earum, amet culpa. Fugit molestiae consectetur optio eligendi perspiciatis. Repudiandae soluta ea odio dignissimos mollitia esse sequi molestiae unde tenetur, explicabo rerum autem laboriosam ab?",
       price: Math.floor(Math.random() * 1000),
